b04bc505cb3a8d8a93c058fe58aa9503568b67de
Cory House <housecor@gmail.com>
2020-03-05 14:07:54 -0600

    handle 404s

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React from "react";
 import Nav from "./Nav";
 import Users from "./Users";
 import Home from "./Home";
-import { Route } from "react-router-dom";
+import PageNotFound from "./PageNotFound";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Nav />
-      <Route path="/" exact>
-        <Home />
-      </Route>
-      <Route path="/users">
-        <Users />
-      </Route>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route>
+          <PageNotFound />
+        </Route>
+      </Switch>
     </>
   );
 }
